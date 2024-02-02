a35510b9ba136a08f93749dfcdac223705715582
Dima Dalloul <50481895+dima-dalloul@users.noreply.github.com>
2024-02-02 23:12:39 +0100

    Fin correction TD1

--- g2-td1.rkt
+++ g2-td1.rkt
@@ -28,17 +28,39 @@
 ; Doubler un mot
 (string-append ch ch)
 
+; Exercice 4
+; carre
+(define (carre x y)
+  (and (and (< x 5) (> x -5))   ; axe des abcisses
+       (and (< y 5) (> y -5))))  ; axe des ordonnées
 
+;cercle
+(define (cercle x y)
+  (< (+ (* x x) (* y y)) 25))
 
+;Demi-plan
+(define (demiplan x y)
+  (< y x))
 
+;Octogone
+(define (octogone x y )
+  (and (and (> x -6) (< x 6))   ;axe des abcisses
+       (and (> y -6) (< y 6))  ; axe des ordonnées
+       (and (and (< y (+ x 9)) (> y (- x 9)))  ; pente à 1
+            (and (< y (+ (- x) 9)) (> y (- (- x) 9))))))  ; pente à -1
 
+;Lignes verticales
+(define (vertical x)
+  (and (< (remainder (- x 1) 4) 1)
+       (> x 0)))  ; x supérieur à 0
 
-
-
-
-
-
-
-
-
-
+;Triangle Rectangle
+(define (trianglerectangle x y)
+  (and (and (> x -5) (> y -3))   ; lignes horizontale et erticale
+       (< (* 13 y) (- 33 (* x 9)))))   ; hypothénuse
+   
+; Exercice 5
+(define (exo5 x1 x2 x3 x4 y1 y2 y3 y4)
+  (not                     ; s'assurer quil ny a pas d'intersection
+    (or (or (> x1 x4) (> x3 x2))
+        (or (> y2 y3) (> y4 y1)))))
